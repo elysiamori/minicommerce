24b3f37f79cc2480ef2b1a29b741463496fe1c90
Valdy Ramadhan <110575212+elysiamori@users.noreply.github.com>
2024-06-19 06:26:12 +0700

    update models

--- internal/api/models/models.go
+++ internal/api/models/models.go
@@ -7,7 +7,7 @@ import (
 type Products struct {
 	ID           uint      `gorm:"primaryKey" json:"id"`
 	ProductName  string    `gorm:"char not null" json:"product_name" valid:"required"`
-	ImageProduct []byte    `gorm:"type:bytea" json:"img_product" valid:"required"`
+	ImageProduct []byte    `gorm:"type:bytea" json:"img_product"`
 	TypeProduct  string    `gorm:"not null" json:"type_product" valid:"required"`
 	Description  string    `gorm:"not null" json:"desc" valid:"required"`
 	Price        int       `gorm:"not null" json:"price" valid:"required"`
