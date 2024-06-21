45bda60f86575d544d72c2be83147571c1a37b74
Valdy Ramadhan <110575212+elysiamori@users.noreply.github.com>
2024-06-21 10:51:01 +0700

    add sold and location data

--- internal/api/dto/request/request.go
+++ internal/api/dto/request/request.go
@@ -5,6 +5,8 @@ type ProductAddRequest struct {
 	ImgProduct  []byte `json:"img_product"`
 	TypeProduct string `json:"type_product" validate:"required"`
 	Desc        string `json:"desc" validate:"required"`
+	Sold        int    `json:"sold" validate:"required"`
+	Location    string `json:"location" validate:"required"`
 	Price       int    `json:"price" validate:"required"`
 	Stock       int    `json:"stock" validate:"required"`
 }
@@ -14,6 +16,8 @@ type ProductUpdatedRequest struct {
 	ImgProduct  []byte `json:"img_product"`
 	TypeProduct string `json:"type_product" validate:"required"`
 	Desc        string `json:"desc" validate:"required"`
+	Sold        int    `json:"sold" validate:"required"`
+	Location    string `json:"location" validate:"required"`
 	Price       int    `json:"price" validate:"required"`
 	Stock       int    `json:"stock" validate:"required"`
 }
